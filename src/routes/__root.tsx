1c0a6523365c3b764f3634f0bbd5f5aa0bf12398
Thomas Sauvage <144670432+cactusninjaa@users.noreply.github.com>
2025-11-16 13:57:23 +0100

    feat/change app name logo favicon
    * feat/add-ci
    
    * feat/add-ci
    
    * fix/button-position-for-mobile
    
    * feat/change app name logo favicon
    
    * fix/delete-unused-import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 // import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 // import { TanStackDevtools } from '@tanstack/react-devtools'
 
-// import Header from '../components/Header'
+import Header from '../components/Header'
 
 import appCss from '../styles.css?url'
 
@@ -17,7 +17,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Dynastie Drive School',
       },
     ],
     links: [
@@ -38,7 +38,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        {/* <Header /> */}
+        <Header />
         {children}
         {/* <TanStackDevtools
           config={{
